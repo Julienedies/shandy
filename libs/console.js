f4b1c96b9dd90549e7d46422c76b0d8d021ef161
Julienedies <julienedies@gmail.com>
2018-07-20 20:49:45 +0800

    添加console管输出理功能

--- libs/console.js
+++ libs/console.js
@@ -1,22 +1,69 @@
 /**
  * Created by j on 18/6/19.
+ * @example
+ * in node:
+ * const cm = require('console-manager');
+ * cm('info','log');  // console.log and console.info 调用后不会有输出
  */
 
-const _console = console;
+;
+(function () {
 
-const _clone = {};
-for(let i in console){
-    if(typeof console[i] == 'function'){
-        _clone[i] = function(){};
-    }
-}
+    const _console = console;
 
-module.exports = {
-    debug: function(bool){
-        if(bool != undefined && !bool){
-            console = _clone;
-        }else{
-            console = _console;
+    const _clone = {};
+
+    const _bak = {};
+
+    const _methods = [];
+
+    for (let i in _console) {
+        if (typeof _console[i] == 'function') {
+            _methods.push(i);
+            _bak[i] = console[i];
+            /*_clone[i] = function(){
+                _console[i].apply(_console, [].slice.call(arguments));
+            }*/
         }
     }
-};
+
+    /*
+     * @todo 管理console的行为,
+     * @param bool {Boolean} [可选] 是否输出console方法调用
+     * @param methods  {String}  [可选]  console方法名
+     */
+    function _export(bool, methods) {
+        let arr = [].slice.call(arguments);
+        bool = arr.shift();
+        methods = arr;
+        if (typeof bool == 'undefined') {
+            bool = false;
+            methods = _methods;
+        }
+        else if (typeof bool == 'boolean') {
+            methods = methods.length ? methods : _methods;
+        }
+        else if (typeof bool == 'string') {
+            methods.unshift(bool);
+            bool = false;
+        }
+        methods.forEach((method) => {
+            console[method] = bool ? _bak[method] : function(){};
+            /*_clone[method] = bool ? function () {
+                _console[method].apply(_console, [].slice.call(arguments));
+            } : function () {
+            };*/
+        });
+
+        //console = _clone;
+    }
+
+
+    // export
+    if(typeof module != 'undefined'){
+        module.exports = _export;
+    } else {
+        window.cm = _export;
+    }
+
+})();
