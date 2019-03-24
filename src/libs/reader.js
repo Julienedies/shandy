e019e23bcc54d0e996da2b612ff41a353e95b602
Julienedies <julienedies@gmail.com>
2019-03-24 22:22:02 +0800

    + reader.js

--- src/libs/reader.js
+++ src/libs/reader.js
@@ -6,28 +6,31 @@ import $ from 'jquery'
 
 let speechSU = new SpeechSynthesisUtterance();
 
-
 class Reader {
     constructor (elm = 'body') {
         let that = this
+
+        this.state = null
         this.elm = elm
         this.list = []
         this.index = 0
+
         speechSU.onend = () => {
-            $(that.list[that.index]).css({color:''})
-            that.index = that.index + 1
-            that.speak()
+            console.log('speechSU.onend')
         }
 
-        $(document).on('click', 'a.reader-readable-node', function (e) {
-            let index = $(this).data('index')
-            that.position(index)
+        window.addEventListener('beforeunload', function (e) {
+            that.cancel()
         })
 
-        this.gui()
+        this._gui(this)
     }
 
-    init () {
+    init (elm) {
+        this.state = 'init'
+        this.list = []
+        this.index = 0
+        this.elm = elm || this.elm
         let dom = $(this.elm)[0]
         walk(dom, this)
 
@@ -44,38 +47,45 @@ class Reader {
                     let $parent = $(node.parentNode)
                     let t = node.nodeValue.trim()
                     if (t.length > 5 && $parent.is(':visible')) {
-                        console.log('#', node.parentNode, node.nodeValue)
+                        //console.log('#', node.parentNode, node.nodeValue)
                         that.list.push(node.parentNode)
-                        $parent.prepend(`<a class="reader-readable-node" style="background:#64c116;color:#fff;padding:5px;" data-index="${ that.list.length - 1 }"> ▶ </a>`)
+                        let index = that.list.length - 1
+                        $parent.prepend(`<a class="reader-readable-node" data-index="${ index }" title="点击播放本段:${ index }"> </a>`)
                     }
                 }
             }
         }
     }
 
-    gui () {
-        let that = this;
-        let $wrapper = $(`<div id="reader-wrapper" style="position:fixed;z-index:10000;bottom:0;left:20px;background:rgba(0,0,0,0.7);padding:3px 7px;"></div>`)
-        let $speakBtn = $(`<a style="color:#fff;">播放</a>`).on('click', function (e) {
-            let $th = $(this)
-            let isPause = $th.attr('data-pause')
+    speak () {
+        let index = this.index
+        let $item = $(this.list[index])
+        speechSU.text = $item.text()
+        speechSynthesis.speak(speechSU)
+        setTimeout(() => {
+            speechSU.onend = () => this.next()
+        }, 3000)
+        this.setState('speak')
+    }
 
-            if (isPause === undefined) {
-                that.init()
-                that.speak()
-                $th.attr('data-pause', 'false')
-                $th.text('暂停')
-            } else if (isPause === 'true') {
-                $th.text('暂停')
-                that.resume()
-                $th.attr('data-pause', 'false')
-            } else {
-                $th.attr('data-pause', 'true')
-                $th.text('播放')
-                that.pause()
-            }
-        })
-        $wrapper.append($speakBtn).appendTo(document.body)
+    cancel () {
+        speechSU.onend = null
+        speechSynthesis.cancel()
+        this.setState('cancel')
+    }
+
+    pause () {
+        speechSynthesis.pause();
+        this.setState('pause')
+    }
+
+    resume () {
+        speechSynthesis.resume();
+        this.setState('resume')
+    }
+
+    volume (n) {
+        speechSU.volume = n // 0.3;
     }
 
     position (index) {
@@ -84,23 +94,102 @@ class Reader {
         this.speak()
     }
 
-    speak () {
-        let index = this.index
-        speechSU.text = $(this.list[index]).css({color:'#64c116'}).text();
-        speechSynthesis.speak(speechSU);
+    next () {
+        let index = this.index + 1;
+        if (index >= this.list.length) {
+            index = 0
+        }
+        this.position(index)
     }
 
-    cancel () {
-        $(this.list[this.index]).css({color:''})
-        speechSynthesis.cancel();
+    setState (state) {
+        console.log('state: ', state, speechSU)
+        this.state = state
+        if (state === 'speak' || state === 'resume') {
+            this.$speakBtn.hide()
+            this.$pauseBtn.show()
+            let $item = $(this.list[this.index]).addClass('reader-reading')
+            $(document).scrollTop($item.offset().top - 15)
+        }
+        if (state === 'pause' || state === 'cancel') {
+            this.$speakBtn.show()
+            this.$pauseBtn.hide()
+            if (state === 'cancel') {
+                $(this.list[this.index]).removeClass('reader-reading')
+            }
+        }
     }
 
-    pause () {
-        speechSynthesis.pause();
+    _gui (that) {
+        this._style()
+        let $reader = $(`<div id="reader-wrapper" style=""></div>`).appendTo(document.body)
+
+        let $speakBtn = $(`<a>播放</a>`).on('click', function (e) {
+            let $th = $(this)
+            if (!that.state) {
+                that.init()
+                that.speak()
+            } else if (that.state === 'pause') {
+                that.resume()
+            } else {
+                that.speak()
+            }
+        }).appendTo($reader)
+
+        let $pauseBtn = $(`<a>暂停</a>`).hide().on('click', function (e) {
+            that.pause()
+        }).appendTo($reader)
+
+        let $volumeBtn = $(`<label>音量 <input type="range" value="7" min="0" max="10"></label>`).on('change', function (e) {
+            let val = $(this).find('input').val()
+            console.log(val)
+            that.volume(val / 10)
+        }).appendTo($reader)
+
+        $(document).on('click', 'a.reader-readable-node', function (e) {
+            let index = $(this).data('index')
+            that.position(index)
+        })
+
+        that.$reader = $reader
+        that.$speakBtn = $speakBtn
+        that.$pauseBtn = $pauseBtn
     }
 
-    resume () {
-        speechSynthesis.resume();
+    _style () {
+        $(`<style>
+            #reader-wrapper{
+                position:fixed;
+                z-index:10000;
+                bottom:0;
+                left:20px;
+                background:rgba(0,0,0,0.7);
+                padding:3px 7px;
+            }
+            #reader-wrapper a, #reader-wrapper label, .reader-readable-node{
+                text-decoration: none!important;
+                color:#fff!important;
+                padding:0 4px!important;
+            }
+            #reader-wrapper label input{
+                display: none;
+            }
+            #reader-wrapper label:hover input{
+                display: inline-block;
+            }
+            .reader-readable-node{
+                background:#64c116!important;
+                display: inline-block!important;
+                width: 1em!important;
+                height: 1em!important;
+                padding: 0!important;
+                margin-right: 0.5em!important;
+                border-radius: 50%!important;
+            }
+            .reader-reading{
+                color:#64c116;
+            }
+        </style>`).appendTo(document.head)
     }
 
 }
