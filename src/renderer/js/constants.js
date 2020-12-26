39281bc48cbe553654aa05af9c210ffdf4c476e9
Julienedies <julienedies@gmail.com>
2020-12-26 23:53:44 +0800

    optimize

--- src/renderer/js/constants.js
+++ src/renderer/js/constants.js
@@ -28,7 +28,7 @@ export const ON_DEL_LOGIC_DONE = 'ON_DEL_LOGIC_DONE';
 export const COLORS_BACKGROUND = ['#ff0000', '#0000ff', '#006400', '#ffff00', '#9400D3', '#000000'];
 export let FroalaEditorConfig = {
     fontSizeDefaultSelection: '16',
-    fontSize: ['14', '16', '18', '20', '22', '24'],
+    fontSize: ['14', '16', '18', '20', '22', '24', '28', '32'],
     colorsBackground: COLORS_BACKGROUND,
 };
 
