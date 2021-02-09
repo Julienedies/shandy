773dde341241504457bb221bbe579ba09a7f198a
Julienedies <julienedies@gmail.com>
2021-02-09 15:49:27 +0800

    optimize

---
+++ src/renderer/pages/stock/system/main.js
@@ -0,0 +1,38 @@
+/**
+ * Created by j on 18/7/28.
+ */
+
+import './index.html'
+import '../../../css/common/common.scss'
+import './style.scss'
+
+import _ from 'lodash'
+import $ from 'jquery'
+import brick from '@julienedies/brick'
+import '@julienedies/brick/dist/brick.css'
+
+import {
+    ADD_LOGIC,
+    EDIT_LOGIC,
+    DEL_LOGIC,
+    ON_DEL_LOGIC_DONE,
+    ON_SET_LOGIC_DONE,
+    ON_GET_LOGIC_DONE,
+    ON_GET_TAGS_DONE,
+    ON_SET_TAG_DONE, ON_DEL_TAG_DONE, DEL_TAG,
+} from '../../../js/constants'
+
+import utils from '../../../js/utils'
+import '../../../js/common-stock.js'
+import Reader from '../../../../libs/reader'
+
+import setTagCtrl from '../../tags/set-tag-ctrl'
+
+brick.reg('setTagCtrl', setTagCtrl);
+
+brick.reg('systemCtrl', function () {
+
+    let scope = this;
+
+
+});
