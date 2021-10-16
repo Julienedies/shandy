7523c27af56ccb9c4e6a0f6540501a129034e2e1
Julienedies <julienedies@gmail.com>
2021-10-16 16:46:28 +0800

    optimize

--- src/server/routes/filter/cross-domain.js
+++ src/server/routes/filter/cross-domain.js
@@ -3,6 +3,7 @@
  */
 
 export default function (req, res, next) {
+    res.header("Access-Control-Allow-Private-Network", true);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
