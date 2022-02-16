670684b1ace4e44fb1d8ec64e31408b764ae92d6
Julienedies <julienedies@gmail.com>
2022-02-17 00:09:08 +0800

    optimize

--- src/renderer/pages/reminder/main.js
+++ src/renderer/pages/reminder/main.js
@@ -20,22 +20,48 @@ import viewerMarkTagCtrl from '../viewer/markTag-ctrl'
 brick.reg('reminderCtrl', function (scope) {
 
     let $memo = $('#memo');
-    $.get('/stock/memo').done(function (o) {
-        $memo.html(o.text);
+
+    let def1 = $.Deferred();
+    let def2 = $.Deferred();
+
+    function getRpForm () {
+        $.get(`/stock/replay?date=${ formatDate2() }`).done((data) => {
+            def1.resolve(data);
+        });
+    }
+
+    function getMemo () {
+        $.get('/stock/memo').done(function (data) {
+            def2.resolve(data);
+        });
+    }
+
+    $.when(def1, def2).done((d1, d2) => {
+        $memo.html(`<pre>${ d1['交易计划'] } ${ d1['交易计划'] }<pre/>${ d2.text }`);
         let reader = new Reader('#memo');
         reader.init();
         reader.autoSpeak();
     });
 
-/*    $.get('/stock/tags/').done((data) => {
-        console.log(data);
-        scope.render('prepare', data);
-        scope.render('mistake', data);
-        //scope.render('logic', data);
-        //scope.render('principle', data);
-    });*/
+    getRpForm();
+    getMemo();
 
-    let timer = 320; // 10分钟，600秒
+    /*    $.get('/stock/memo').done(function (o) {
+            $memo.html(o.text);
+            let reader = new Reader('#memo');
+            reader.init();
+            reader.autoSpeak();
+        });*/
+
+    /*    $.get('/stock/tags/').done((data) => {
+            console.log(data);
+            scope.render('prepare', data);
+            scope.render('mistake', data);
+            //scope.render('logic', data);
+            //scope.render('principle', data);
+        });*/
+
+    let timer = 420; // 10分钟，600秒
     let $timer = $('#timer');
 
     setInterval(() => {
