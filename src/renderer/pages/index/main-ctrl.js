8e37ce8dac5aef17b43767244becb62714a54d50
julienedies <julienedies@gmail.com>
2025-09-06 10:10:27 +0800

    optimize

--- src/renderer/pages/index/main-ctrl.js
+++ src/renderer/pages/index/main-ctrl.js
@@ -373,7 +373,7 @@ brick.reg('mainCtrl', function (scope) {
 
     if (utils.isTradingDate()) {
         utils.timer('9:00', () => {
-            //scope.openReminder();
+            scope.openReminder();
         });
         utils.timer('12:45', () => {
             //scope.openReminder();
--- src/renderer/pages/reminder/main.js
+++ src/renderer/pages/reminder/main.js
@@ -20,22 +20,59 @@ import viewerMarkTagCtrl from '../viewer/markTag-ctrl'
 brick.reg('reminderCtrl', function (scope) {
 
     let $memo = $('#memo');
-    $.get('/stock/memo').done(function (o) {
-        $memo.html(o.text);
+
+    let def1 = $.Deferred();
+    let def2 = $.Deferred();
+
+    function getRpForm () {
+        $.get(`/stock/replay?date=${ formatDate2() }`).done((data) => {
+            def1.resolve(data);
+        });
+    }
+
+    function getMemo () {
+        $.get('/stock/memo').done(function (data) {
+            def2.resolve(data);
+        });
+    }
+
+
+    $.when(def1, def2).done((d1, d2) => {
+        let mq = `
+        做着做着就忘记了教训和悔恨，沉迷于无效的日常交易，重复所有错误；没有任何改变;
+        做着做着就忘记了教训和悔恨，沉迷于无效的日常交易，重复所有错误；没有任何改变；
+        为何我错过所有的大行情和大龙头？为何我错过所有的大行情和大龙头？
+        无系统，无计划；
+        没有大局观，着眼于小波动；
+        主观与线性思维；
+        客服不了本能，控制不住情绪；
+        `;
+        let plan = d1['交易计划'] || '如果没有明确的交易计划，就不要交易; 无系统、无计划、临盘随意交易是我最大的亏损来源';
+        $memo.html(`<pre>${ mq } </pre><pre>${ plan } </pre> <pre>${ plan }</pre> <pre>${ plan }</pre>  ${ d2.text }`);
         let reader = new Reader('#memo');
         reader.init();
         reader.autoSpeak();
     });
 
-    $.get('/stock/tags/').done((data) => {
-        console.log(data);
-        scope.render('prepare', data);
-        scope.render('mistake', data);
-        //scope.render('logic', data);
-        //scope.render('principle', data);
-    });
+    getRpForm();
+    getMemo();
 
-    let timer = 140; // 10分钟，600秒
+    /*    $.get('/stock/memo').done(function (o) {
+            $memo.html(o.text);
+            let reader = new Reader('#memo');
+            reader.init();
+            reader.autoSpeak();
+        });*/
+
+    /*    $.get('/stock/tags/').done((data) => {
+            console.log(data);
+            scope.render('prepare', data);
+            scope.render('mistake', data);
+            //scope.render('logic', data);
+            //scope.render('principle', data);
+        });*/
+
+    let timer = 240; // 秒
     let $timer = $('#timer');
 
     setInterval(() => {
